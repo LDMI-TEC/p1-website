1d24245b0160911c9eaeecdc310f4c46c837e698
Halfdan <halfdansc@gmail.com>
2025-04-23 13:23:47 +0200

    added the Author model and the Categories model

---
+++ Models/Jokes/Authors.cs
@@ -0,0 +1,9 @@
+namespace poke_poke.Models.Jokes
+{
+    public class Authors
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ Models/Jokes/Categories.cs
@@ -0,0 +1,8 @@
+namespace poke_poke.Models.Jokes
+{
+    public class Categories
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+    }
+}
